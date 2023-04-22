8dbc80ea53faaa5c768791a63f3dde3a72d6a55d
tomsan96 <gradada0507@gmail.com>
2023-04-22 17:36:02 +0900

    first commit

---
+++ CalculationApp/CalculationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculationAppApp.swift
+//  CalculationApp
+//
+//  Created by 山崎定知 on 2023/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
