eb6743f035af808d3aaa108ab5b3dd0d03c5ff54
tomsan96 <gradada0507@gmail.com>
2023-04-24 20:10:04 +0900

    docsフォルダを作成して利用規約ページを追加 アプリ上から免責条項の削除

--- CalculationApp/Views/ContentView.swift
+++ CalculationApp/Views/ContentView.swift
@@ -12,7 +12,6 @@ struct ContentView: View {
     @State var finalTotalAmount: Int?
     @State var capital: Int?
     @State var investmentIncome: Int?
-    @State var isShowingSheet = false
     @FocusState var isTextFieldFocused: UUID?
     var isDisabledCalculateButton: Bool {
         return investmentAsset.initialInvestmentAmountWithTenThousandYen == nil
@@ -39,12 +38,6 @@ struct ContentView: View {
             OutputItemView(unit: .yen, title: "将来の資産合計金額", number: $finalTotalAmount)
             OutputItemView(unit: .yen, title: "元本", number: $capital)
             OutputItemView(unit: .yen, title: "運用収益", number: $investmentIncome)
-            Button("免責事項") {
-                isShowingSheet.toggle()
-            }
-            .sheet(isPresented: $isShowingSheet) {
-                DisclaimerView()
-            }
             .padding(.top, 12)
         }
         .padding()
